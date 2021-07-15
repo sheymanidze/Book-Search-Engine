1fdfc07685f9a39a9cd18c491f59d144be27c3fe
Anastasia Sorkin <sorkin.anastasia@gmail.com>
2021-07-15 12:18:15 -0400

    mutation login added

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -26,6 +26,21 @@ const resolvers = {
       const token = signToken(user);
       return {token, user};
     },
+    login: async (parent, {email, password}) => {
+      const user = await User.findOne({email});
+
+      if (!user) {
+        throw new AuthenticationError('No user found with this email address');
+      }
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+      const token = signToken(user);
+
+      return { token, user };
+    },
     saveBook: async (parent, {bookData}, context) => {
       if (context.user) {
         const updateUser = await User.findByIdAndUpdate(
