332c6c184ddb15dd3faed4cb37fa03994f2ca9cc
Anastasia Sorkin <sorkin.anastasia@gmail.com>
2021-07-15 19:54:11 -0400

    build added, some errors fixed

---
+++ client/build/precache-manifest.61d35eeb78cf52cbc7f3cb48c5e4ea3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46aa5db0a353d23657cef2ce16f060d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a18edfc289620c05de58",
+    "url": "/static/css/2.63f31f8d.chunk.css"
+  },
+  {
+    "revision": "279d88ed57931dbd11a1",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "a18edfc289620c05de58",
+    "url": "/static/js/2.74d32049.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/static/js/2.74d32049.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "279d88ed57931dbd11a1",
+    "url": "/static/js/main.66b3c257.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
